c9351f45515131f9946e7207ebb36d7254ce0288
Tom <tomw08@gmail.com>
2017-07-30 11:58:32 +0100

    Prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,67 +1,66 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+import axios from "axios";
+import cheerio from "cheerio";
 
-const isString = (myVar) =>  typeof myVar === 'string' || myVar instanceof String
+const isString = myVar => typeof myVar === "string" || myVar instanceof String;
 
 // verifies that action should be handled by middleware
-export function isCheerioTask(action){
-		try {
-      // TODO MOVE INTO OWN FUNC
-      if(typeof action.payload.task === 'function'  
-         && isString(action.payload.url) === true
-         && action.type === "CHEERIO_TASK") {
-        return true
-      }
-   }
-   catch(e){
-    if(e instanceof TypeError === true){
-      return false
+export function isCheerioTask(action) {
+  try {
+    if (
+      typeof action.payload.task === "function" &&
+      isString(action.payload.url) === true &&
+      action.type === "CHEERIO_TASK"
+    ) {
+      return true;
     }
-   }
-    return false
+  } catch (e) {
+    if (e instanceof TypeError === true) {
+      return false;
+    }
+  }
+  return false;
 }
 
+const createCheerioMiddleware = () => ({
+  dispatch,
+  getState
+}) => next => action => {
+  if (isCheerioTask(action) === true) {
+    const pendingAction = {
+      type: `${action.type}_PENDING`,
+      payload: action.payload
+    };
 
-function createCheerioMiddleware() {
-	return ({ dispatch, getState }) => next => action => {
+    dispatch(pendingAction);
 
-	  if (isCheerioTask(action)===true){
-      const pendingAction = {
-        type: `${action.type}_PENDING`,
-        payload: action.payload
-      }
+    return axios
+      .get(action.payload.url)
+      .then(response => {
+        const $ = cheerio.load(response.data);
+        const parsedData = action.payload.task($);
+        const fulfilledAction = {
+          type: `${action.type}_FULFILLED`,
+          payload: {
+            parsedData
+          }
+        };
 
-      dispatch(pendingAction)
-      
-      return axios.get(action.payload.url)
-            .then(response => {
-              let $ = cheerio.load(response.data)
-              let parsedData = action.payload.task($)
-              const fulfilledAction = {
-                type: `${action.type}_FULFILLED`, 
-                payload: { 
-                  parsedData 
-                }
-              }
+        dispatch(fulfilledAction);
+      })
+      .catch(err => {
+        const rejectedAction = {
+          type: `${action.type}_REJECTED`,
+          payload: {
+            err
+          }
+        };
 
-              dispatch(fulfilledAction)
+        dispatch(rejectedAction);
+      });
+  }
 
-            })
-            .catch(err => {
-                const rejectedAction = {
-                type: `${action.type}_REJECTED`, 
-                payload: { 
-                  err 
-                }
-              }
-
-              dispatch(rejectedAction)
-            });
-     }
-
-		return next(action);
-	}
-}
+  return next(action);
+};
 
 const cheerioMiddleware = createCheerioMiddleware();
 
